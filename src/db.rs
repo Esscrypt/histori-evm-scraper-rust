37aebaf7a0d584eb42e3ac2023c60c6c5e12d3b7
Mihail Kirov <me@mikirov.xyz>
2024-09-30 23:45:00 +0300

    WIP

--- src/db.rs
+++ src/db.rs
@@ -12,6 +12,7 @@ pub fn establish_connection_pool() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     Pool::builder()
+        .max_size(100)
         .build(manager)
         .expect("Failed to create pool.")
 }
