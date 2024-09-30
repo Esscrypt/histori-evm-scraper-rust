30af9dc8936ea6d095ede453b650253789b2591c
Mihail Kirov <me@mikirov.xyz>
2024-09-30 19:07:17 +0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -163,7 +163,7 @@ async fn fetch_logs(provider: &Provider<Http>, from_block: u64, to_block: u64, c
 
     // Convert HashSet back to Vec<H256> for the FilterBuilder
     let topics_vec: Vec<H256> = topics.into_iter().collect();
-    info!("Fetching logs with topics: {:?}", topics_vec);
+    // info!("Fetching logs with topics: {:?}", topics_vec);
     // Build the log filter
     let filter = Filter::new()
         .from_block(BlockNumber::Number(from_block.into()))
@@ -171,10 +171,13 @@ async fn fetch_logs(provider: &Provider<Http>, from_block: u64, to_block: u64, c
         .events(topics_vec);
 
     let logs = match provider.get_logs(&filter).await {
-        Ok(logs) => logs,
+        Ok(logs) => {
+            info!("Fetched {} logs", logs.len());
+            logs
+        },
         Err(e) => {
             error!("Error fetching logs: {:?}", e);
-            return Vec::new();
+            Vec::new()
         },
     };
     logs
